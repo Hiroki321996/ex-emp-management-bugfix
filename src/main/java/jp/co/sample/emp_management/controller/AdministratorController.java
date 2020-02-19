ae8d270ed32dd69acbfafb78c00340f23cc07210
Hiroki321996 <hiroki.tani@rakus-partners.co.jp>
2020-02-19 17:38:55 +0900

    1-3: メールアドレスの重複があるときにエラーメッセージを出すようにしました

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -70,6 +71,12 @@ public class AdministratorController {
 	 */
 	@RequestMapping("/insert")
 	public String insert(@Validated InsertAdministratorForm form,BindingResult result) {
+		
+		Administrator admin = administratorService.findByMailAddress(form.getMailAddress());
+		if(admin != null) {
+			FieldError fieldError = new FieldError(result.getObjectName(), "mailAddress", "メールアドレスが重複しています。");
+			result.addError(fieldError);
+		}
 		if(result.hasErrors()) {
 			return toInsert();
 		}

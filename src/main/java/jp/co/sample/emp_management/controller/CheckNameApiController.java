6b6feb945bfc5e2871350f6478a21af6c617b35c
Hiroki321996 <hiroki.tani@rakus-partners.co.jp>
2020-02-21 10:45:00 +0900

    JavaDocコメントを書き加えました

--- src/main/java/jp/co/sample/emp_management/controller/CheckNameApiController.java
+++ src/main/java/jp/co/sample/emp_management/controller/CheckNameApiController.java
@@ -12,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 import jp.co.sample.emp_management.domain.Employee;
 import jp.co.sample.emp_management.service.EmployeeService;
 
+/**
+ * 従業員検索のオートコンプリートのためのAPI
+ * 
+ * @author sanihiro
+ *
+ */
 @RestController
 @RequestMapping(value = "/check_name_api")
 public class CheckNameApiController {
@@ -19,6 +25,12 @@ public class CheckNameApiController {
 	@Autowired
 	private EmployeeService employeeService;
 	
+	/**
+	 * 名前で曖昧検索をし出てきた従業員情報から名前の配列を作り出してmapに格納します
+	 * 
+	 * @param name 検索欄に入力された名前
+	 * @return 従業員の名前の配列をいれたmap
+	 */
 	@RequestMapping(value = "/namecheck", method = RequestMethod.POST)
 	public Map<String, String[]> namecheck(String name){
 		Map<String, String[]> map = new HashMap<>();
